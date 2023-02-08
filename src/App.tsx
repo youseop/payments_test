2eeb3c0f87d34dd46736aefe8965e624000e5d48
youseop <edlsw@naver.com>
2023-02-08 17:31:23 +0900

    add https settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">toss payments</div>;
+  return (
+    <div className="App">
+      <h1>toss payments</h1>
+      <div>{process.env.REACT_APP_CLIENT_KEY}</div>
+      <div>{process.env.REACT_APP_SECRET_KEY}</div>
+    </div>
+  );
 }
 
 export default App;
