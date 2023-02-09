f5bb309e2c914acc31e0e2e4eebd099343126ccc
youseop <edlsw@naver.com>
2023-02-09 18:21:57 +0900

    add basic components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+import "../css/Footer.scss";
+
+const Footer = () => {
+  return <div className="footer">footer</div>;
+};
+
+export default Footer;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+import "../css/Navbar.scss";
+
+const Navbar = () => {
+  return <div className="navbar">navbar unknown ocean</div>;
+};
+
+export default Navbar;
