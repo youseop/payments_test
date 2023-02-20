22a643a336e395f62b83b68dc6d1b9df70ee2e61
youseop <edlsw@naver.com>
2023-02-20 20:54:27 +0900

    basic setting of firebase google oauth

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,20 @@
 import "../css/Navbar.scss";
+import { handleGoogleLogin } from "../util/firebase";
 
 const Navbar = () => {
-  return <div className="navbar">navbar unknown ocean</div>;
+  return (
+    <div className="navbar">
+      <div>navbar unknown ocean</div>
+      <div
+        onClick={async () => {
+          const data = await handleGoogleLogin();
+          console.log("data", data);
+        }}
+      >
+        login
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
